844838c31cc0761ba91952ea589a01b9ccdc79c3
Mirko Morana <m.morana094@gmail.com>
2025-08-08 02:13:03 +0200

    feat: correzione refusi

--- script/azioni.js
+++ script/azioni.js
@@ -1,7 +1,7 @@
 
 /*
-PLUGIN Lugantes - 05 Aprile 2020 - 09 Giugno 2020
-In costruzione - basato su Script originale di WilliamJ
+PLUGIN Dastian - 07 Agosto 2025 
+In costruzione - basato su Script di Lugantes
 Da sistemare i commenti e l'impaginazione per rendere il file leggibile
 
 
@@ -28,9 +28,6 @@ function carica_plugin() {
 					"<div class='comando' id='apriazioncine' title='Apri Azioni'><i class='fa fa-diamond'></i></div>"+
 					//"<div class='comando' id='mapparapida' title='Mappa Rapida'><i class='fa fa-map-signs'></i></div>"+
 					"<div class='comando' id='miascheda' title='Apri Scheda'><i class='fa fa-id-badge'></i></div>"+
-
-					"<div class='comando' id='botteghe' title='Apri Botteghe'><i class='fa fa-shopping-bag'></i></div>"+
-
 					"<div class='comando' id='scelto_forum' title='Apri Bacheche'><i class='fa fa-clipboard'></i></div>"+
 					"<div class='comando' id='apri_online' title='Elenco online'><i class='fa fa-users'></i></div>"+ 
 					"<div class='comando' id='leggiposta' title='Posta'><i class='fa fa-envelope'></i></div>"+
@@ -152,6 +149,7 @@ $(document).ready(function() {
 })
 // Banca
 .on('click','#banca',function(e) { finestra('Banca','Banca di Lot','https://extremeplug.altervista.org/docs/plugin/altri.php?link=https://www.extremelot.eu/lotnew/banca_d.asp','width=90%,height=550'); 
+
 // ----chiusura dialog mappe
 $(document).ready(function() {
          var dialogOptions = {
@@ -169,7 +167,6 @@ $(document).ready(function() {
 
       }); 
 })
-//
 
 /*MISSIVA*/
 .on('click','#scriviposta, [myid="scriviposta"]',function(e) { scriviposta(); 
@@ -484,50 +481,15 @@ function aziona(id) { top.result.location='../proc/azioni.asp?azione='+id; }
 
 /*----vediamo quanto siamo belli*/
 function vedischeda() {
-  // Controlla se il dialogo esiste già
-  if ($("#confermaSchedaDialog").length) {
-    $("#confermaSchedaDialog").remove();
-  }
-
-  // Crea il dialogo
-  const dialogHtml = `
-    <div id="confermaSchedaDialog" title="Vedi Scheda?">
-      <p>Vuoi vedere la tua scheda?</p>
-    </div>
-  `;
-  $('body').append(dialogHtml);
-
-  // Costruzione dialog
-  $("#confermaSchedaDialog").dialog({
-    modal: true,
-    buttons: {
-      "Sì": function () {
-        $(this).dialog("close");
-
-        // Prende il nome del pg dal DOM
-        const nome = $("#datidelpg input[name='player']").val();
-        if (!nome) {
-          alert("Nome PG non trovato.");
-          return;
-        }
-
-        apriScheda(nome);
-      },
-      "No": function () {
-        $(this).dialog("close");
-
-        // Prompt per inserire il nome manualmente
-        const nome = prompt("Inserisci il nome del personaggio:");
-        if (!nome) return;
-
-        apriScheda(nome);
-      }
-    },
-    close: function () {
-      $(this).remove();
-    }
-  });
-
+  // Prompt diretto per il nome del personaggio
+  const nome = prompt("Inserisci il nome del personaggio:");
+  
+  // Se non inserisce niente o annulla, esce
+  if (!nome) return;
+  
+  // Chiama la funzione per aprire la scheda
+  apriScheda(nome);
+  
   // Funzione per aprire la scheda
   function apriScheda(nome) {
     const pulito = nome.replace(/[^\w\-]/g, '');
@@ -537,38 +499,6 @@ function vedischeda() {
       'width=1000,height=600');
   }
 }
-
-function apriBotteghe() {
-  finestra(
-    'bottegheLot',
-    'Botteghe di Lot',
-    'https://extremeplug.altervista.org/docs/plugin/altri.php?link=https://www.extremelot.eu/lotnew/botteghe.asp',
-    'width=90%,height=550'
-  );
-}
-
-$(document).on('click', '#botteghe', function (e) {
-  e.preventDefault();
-  apriBotteghe();
-
-  // ---- chiusura/apertura dialog botteghe
-  $(document).ready(function () {
-    var dialogOptions = {
-      autoOpen: false
-    };
-
-    $("#dlg-bottegheLot").dialog(dialogOptions);
-
-    $("#botteghe").click(function () {
-      if (!$("#dlg-bottegheLot").dialog("isOpen")) {
-        $("#dlg-bottegheLot").dialog("open");
-      } else {
-        $("#dlg-bottegheLot").dialog("close");
-      }
-    });
-  });
-});
-
 
 function nofarniente() { } 
 
