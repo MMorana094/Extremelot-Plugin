67163e62e053e9cc0f1bd46a4d54d9b733a1076b
Mirko Morana <m.morana094@gmail.com>
2025-08-26 18:59:11 +0200

    fix

--- background.js
+++ background.js
@@ -4,13 +4,11 @@ chrome.runtime.onInstalled.addListener(function (details) {
 
     if (details.reason === "install") {
       console.info("First version installed");
-      // Potresti mostrare un messaggio di benvenuto da popup
     } else if (details.reason === "update") {
       console.info("Updated version: " + thisVersion);
 
       chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
         tabs.forEach(tab => {
-          // Assicurati che la pagina sia quella su cui gira il content script
           if (tab.url && tab.url.includes("extremelot.eu/lotnew/_index2.htm")) {
             chrome.tabs.sendMessage(tab.id, {
               name: "showPopupOnUpdated",
