bdcd3f091a83b9cba160833db3d4d84dd0f68d66
MMorana094 <m.morana094@gmail.com>
2026-02-10 01:32:04 +0100

    refactor: rimosso proxy esterno

--- script/core/constants.js
+++ script/core/constants.js
@@ -1,22 +1,12 @@
 // core/constants.js
 (function (w) {
   const origin = (w.location && w.location.origin) ? w.location.origin : 'https://www.extremelot.eu';
-  const EXTREMEPLUG_ORIGIN = 'https://extremeplug.altervista.org';
 
   w.ExtremePlug = w.ExtremePlug || {};
-
-  function proxyUrl(link, options) {
-    const params = new URLSearchParams();
-    if (options && options.classe) params.set('classe', options.classe);
-    params.set('link', link);
-    return EXTREMEPLUG_ORIGIN + '/docs/plugin/altri.php?' + params.toString();
-  }
 
   w.ExtremePlug.constants = {
     ORIGIN: origin,
     LOTNEW_BASE: origin + '/lotnew/',
     PROC_BASE: origin + '/proc/',
-    EXTREMEPLUG_ORIGIN: EXTREMEPLUG_ORIGIN,
-    proxyUrl: proxyUrl
   };
 })(window);
